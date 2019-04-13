ae5f4f49ffe2f4c1348dee07d377024ea3ac0dfe
RTBMBuildUp <e1n18088@oit.ac.jp>
2019-04-13 10:56:16 +0900

    wizzardのflucstrateStatusの再定義

--- scala/Creature/Job/Wizard.scala
+++ scala/Creature/Job/Wizard.scala
@@ -2,6 +2,7 @@ package Creature.Job
 
 import Creature.{Creature, CreatureUtility, Person}
 import Effector.{Effector, Spell}
+import Identifilable.Identifilable
 import _root_.Effector.Equipment.Equipment
 import Status.Status
 
@@ -9,6 +10,19 @@ class Wizard(_name: String, _status: Status, equipment: Equipment, _spellLst: Li
   override def identify: String = "wizard"
 
   override def spellLst: List[Spell] = _spellLst
+
+  override def flucstrateStatus(identificatable: Identifilable, func: Int => Int): Wizard = {
+    val lst = _status.intMap.unzip._2.toList
+
+    Wizard(
+      _name,
+      Status((func(_status.intMap(identificatable.identify)) :: lst.reverse).reverse),
+      equipment,
+      spellLst,
+      effectLst
+    )
+  }
+
 
   override def chant(spell: Effector, target: Creature, participantMap: Map[String, Creature]): Map[String, Creature] =
     participantMap + CreatureUtility.creatureToMapElem(target.addEffect(spell))
